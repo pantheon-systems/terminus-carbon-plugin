1de95156bb0b2ea1ee2b125367162ab6a468a4ba
bhavanvir <bhavanvirrai@pantheon.io>
2026-06-15 11:30:39 -0700

    ci: migrate from CircleCI to GHA
    fix: test case failures
    
    fix: skip functional tests if there are none
    
    style: update `if` statement
    
    fix: remove unused tests
    
    fix: skip tests if there are none

--- src/Model/CarbonSiteEnv.php
+++ src/Model/CarbonSiteEnv.php
@@ -4,9 +4,7 @@ namespace Pantheon\TerminusCarbon\Model;
 
 use Pantheon\Terminus\Models\TerminusModel;
 
-
 class CarbonSiteEnv extends TerminusModel
 {
     const PRETTY_NAME = 'carbon site environment';
-
 }
